741c07a38a9d9886437ab5b5d0b4bd74de081bd7
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-24 17:09:22 +0300

    Destructuring App

--- App.tsx
+++ App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {Quotes} from './src/screens/Quotes';
 
-const App = () => {
-  return (
-      <>
-        <StatusBar barStyle="dark-content" />
-        <Quotes />
-      </>
-  );
-};
+const App = () => (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Quotes />
+    </>
+);
 
 export default App;
