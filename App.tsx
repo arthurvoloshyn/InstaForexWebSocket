eb1b1f6469c7b8f4f1243d2479bd8e039e53573b
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-25 23:09:04 +0300

    Add fonts config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React, {FC} from 'react';
 import {StatusBar} from 'react-native';
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
+import Files from './src/constants/files';
 import Quotes from './src/screens/Quotes';
+import ErrorBoundary from './src/components/ErrorBoundary';
 
-const App: FC = () => (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <Quotes />
-    </>
-);
+const App: FC = () => {
+    const [fontsLoaded] = useFonts(Files.fonts);
+
+    if (!fontsLoaded) return <AppLoading />;
+
+    return (
+        <ErrorBoundary>
+            <StatusBar barStyle="dark-content"/>
+            <Quotes/>
+        </ErrorBoundary>
+    );
+};
 
 export default App;
