be0eae7ac662d40cc6c8286bd4477242c6969b84
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-27 14:45:19 +0300

    Fix bug with showing navbar in Web

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
                 borderBottomColor: Themes.primaryColor,
                 borderBottomWidth: 1,
             },
-            android: {
+            default: {
                 backgroundColor: Themes.primaryColor,
             },
         }),
