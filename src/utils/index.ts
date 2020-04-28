47c19bc4f5d702d41a4806505d4ad24c826438a0
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-28 22:55:18 +0300

    Update sortBySymbol util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 import {IQuota, IQuotaMap} from "../types";
 
-export const sortBySymbol = (data: IQuotaMap): IQuota[] => Object.values(data).sort((a: IQuota, b: IQuota): number => a.symbol.localeCompare(b.symbol));
+export const sortBySymbol = (data: IQuotaMap): IQuota[] => {
+    const dataValues: IQuota[] = Object.values(data);
+
+    return dataValues.sort((a: IQuota, b: IQuota): number => a.symbol.localeCompare(b.symbol));
+};
