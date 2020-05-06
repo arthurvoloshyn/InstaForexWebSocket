e2d4853da80f20384d072e99838925924696e08e
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-05-06 19:13:54 +0300

    Update IProps for custom statusBar

--- src/views/components/AppStatusBar/types.ts
+++ src/views/components/AppStatusBar/types.ts
@@ -2,7 +2,6 @@ import { StatusBarProps, ViewStyle } from "react-native";
 
 export interface IProps extends StatusBarProps {
     backgroundColor?: string;
-    translucent?: boolean;
 }
 
 export type IStyle = {
