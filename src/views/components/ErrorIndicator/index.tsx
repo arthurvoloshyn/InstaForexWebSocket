02af818a96623072d2fd2382fc7bbcc209355f6f
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-05-06 19:29:13 +0300

    Update ErrorIndicator component

--- src/views/components/ErrorIndicator/index.tsx
+++ src/views/components/ErrorIndicator/index.tsx
@@ -1,18 +1,13 @@
 import React, { FC } from 'react';
 import { View } from 'react-native';
-import {IProps} from "./types";
 import AppText from '../AppText';
 import styles from './styles';
 
-const ErrorIndicator: FC<IProps> = ({ children }) => (
+const ErrorIndicator: FC = ({ children = null }) => (
   <View style={styles.error}>
     <AppText style={styles.text}>Something went wrong!</AppText>
     {children}
   </View>
 );
-
-ErrorIndicator.defaultProps = {
-  children: null,
-};
 
 export default ErrorIndicator;
--- src/views/components/ErrorIndicator/types.ts
+++ src/views/components/ErrorIndicator/types.ts
@@ -1,9 +1,4 @@
-import { ReactNode } from "react";
 import { TextStyle, ViewStyle } from "react-native";
-
-export type IProps = {
-    children?: ReactNode,
-};
 
 export type IStyle = {
     error: ViewStyle,
