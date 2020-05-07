562502dc63d84e6c6cdab28958a34ffeb3fa5944
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-05-07 19:53:00 +0300

    Add main file to root folder of mocks

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,10 +1,3 @@
-import { IQuote } from "../types/quote";
+import { mockQuoteMsg } from './quote';
 
-export const mockQuoteMsg: IQuote = {
-    symbol: 'Unknown',
-    ask: 0,
-    bid: 0,
-    change: 0,
-    digits: 0,
-    lasttime: Date.now(),
-};
+export { mockQuoteMsg };
